5a12026e085f3fd39cf71ca6f22d3c8c49d87a93
Jesse Akozbek <jaakozbek@gmail.com>
2020-05-22 20:00:40 -0500

    Added page where users can calculate abv dilution

---
+++ src/components/home.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../apothecary.png';
+import {Link} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/dilute-alcohol-calculator">Calculate ABV</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Home;
