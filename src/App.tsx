5cd099ca9d03cf61a54a1c3c7be96c0480fd2f67
Jesse Akozbek <jaakozbek@gmail.com>
2020-08-02 14:28:41 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route , Switch} from 'react-router-dom';
 import DiluteAlcoholCalculator from './components/dilute-alcohol-calc';
 import CalculateApothecaryCost from './components/calculate-apothecary-cost';
+import VitaminCTitration from './components/vitamin-c-titration';
 import Home from './components/home';
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path='/dilute-alcohol-calculator' component={DiluteAlcoholCalculator} />
           <Route path='/calculate-apothecary-cost' component={CalculateApothecaryCost} />
+          <Route path='/vit-c' component={VitaminCTitration} />
         </Switch>
       </header>
     </div>
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import logo from '../apothecary.png';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <nav className="navbar navbar-light">
+      <img src={logo} className="App-logo" alt="logo" />
+      <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
           <li>
-            <Link to="/dilute-alcohol-calculator">Calculate ABV</Link>
+            <Link to="/dilute-alcohol-calculator">
+              Calculate ABV
+            </Link>
           </li>
           <li>
             <Link to="/calculate-apothecary-cost">Calculate Apothecary Cost</Link>
+          </li>
+          <li>
+            <Link to="/vit-c">Vitamin C Titration Calculator</Link>
           </li>
         </ul>
       </nav>
